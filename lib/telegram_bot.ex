2354968951f07fe7c9342222aa20e7820012a445
chenxsan <chenxsan@gmail.com>
2017-10-20 22:01:09 +0800

    add getMe & sendMessage

--- lib/telegram_bot.ex
+++ lib/telegram_bot.ex
@@ -1,18 +1,23 @@
 defmodule TelegramBot do
-  @moduledoc """
-  Documentation for TelegramBot.
-  """
-
+  import TelegramBot.API, only: [http: 1, http: 2]
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> TelegramBot.hello
-      :world
-
+  A simple method for testing your bot's auth token. Requires no parameters. Returns basic information about the bot in form of a User object.
   """
-  def hello do
-    :world
+
+  @typep telegram_response :: {:ok, map} | {:error, String.t}
+
+  @spec getMe() :: telegram_response
+  def getMe do
+    http("/getMe")
   end
+
+  @typep sendMessageOption :: [parse_mode: String.t, disable_web_page_preview: boolean, disable_notification: boolean, reply_to_message_id: integer]
+  
+  @spec sendMessage(String.t | integer, String.t, sendMessageOption) :: telegram_response
+  def sendMessage(chat_id, text, options \\ []) do
+    body = options ++ [chat_id: chat_id, text: text]
+    |> Enum.into(%{})
+    http("/sendMessage", body)
+  end
+
 end
---
+++ lib/telegram_bot_api.ex
@@ -0,0 +1,60 @@
+defmodule TelegramBot.API do
+  use HTTPoison.Base
+  
+  defp token, do: Application.get_env(:telegram_bot, :token)
+
+  defp proxy do
+    cond do
+      Application.get_env(:telegram_bot, :proxy) -> Application.get_env(:telegram_bot, :proxy)
+      System.get_env("http_proxy") -> System.get_env("http_proxy")
+      System.get_env("https_proxy") -> System.get_env("https_proxy")
+      true -> nil
+    end
+  end
+
+  def process_url(method_name) do
+    "https://api.telegram.org/bot" <> token() <> method_name
+  end
+
+  def process_request_options(options) do
+    if proxy() do Keyword.merge(options, [proxy: proxy()]) else options end
+  end
+
+  def process_request_body(body) do
+    Poison.encode! body
+  end
+
+  def process_response_body(body) do
+    Poison.decode! body
+  end
+
+  defp handle_response(response) do
+    case response do
+      {:ok, %HTTPoison.Response{
+        body: %{
+          "ok" => true,
+          "result" => result
+        }
+      }} -> {:ok, result}
+      {:ok, %HTTPoison.Response{
+        body: %{
+          "ok" => false,
+          "description" => reason
+        }
+      }} -> {:error, reason}
+      {:error, %HTTPoison.Error{
+        reason: reason
+      }} -> {:error, reason}
+    end
+  end
+
+  def http(action) do
+    post(action, %{}, [{"Content-Type", "application/json"}], [])
+    |> handle_response
+  end
+  
+  def http(action, data) do
+    post(action, data, [{"Content-Type", "application/json"}], [])
+    |> handle_response
+  end
+end
