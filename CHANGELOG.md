b419ce74c884e589478d8d989ebade21d2422df0
chenxsan <chenxsan@gmail.com>
2018-03-22 13:39:23 +0800

    upgrade httpoison

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.1.3
+
+1. Upgrade `httpoison` depdency
+
 ## 0.1.2
 
 1. add `getFile`
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TelegramBot.Mixfile do
   def project do
     [
       app: :telegram_bot,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.4",
       start_permanent: Mix.env == :prod,
       deps: deps(),
@@ -24,7 +24,7 @@ defmodule TelegramBot.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:httpoison, "~> 0.13"},
+      {:httpoison, "~> 1.0"},
       {:jason, "~> 1.0"},
       {:ex_doc, ">= 0.0.0", only: :dev}
     ]
