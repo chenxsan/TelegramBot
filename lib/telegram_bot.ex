af5abc32649c6c1821608c2abd5386011a08c96a
chenxsan <chenxsan@gmail.com>
2018-03-22 11:20:52 +0800

    add getFile

--- lib/telegram_bot.ex
+++ lib/telegram_bot.ex
@@ -1,23 +1,46 @@
 defmodule TelegramBot do
+  @moduledoc """
+  A simple Telegram Bot API wrapper for elixir.
+  """
+
   import TelegramBot.API, only: [http: 1, http: 2]
+
   @doc """
   A simple method for testing your bot's auth token. Requires no parameters. Returns basic information about the bot in form of a User object.
   """
 
-  @typep telegram_response :: {:ok, map} | {:error, String.t}
+  @type telegram_response :: {:ok, map} | {:error, String.t()}
 
   @spec getMe() :: telegram_response
   def getMe do
     http("/getMe")
   end
 
-  @typep sendMessageOption :: [parse_mode: String.t, disable_web_page_preview: boolean, disable_notification: boolean, reply_to_message_id: integer]
-  
-  @spec sendMessage(String.t | integer, String.t, sendMessageOption) :: telegram_response
+  @typedoc """
+  Value could either be `HTML` or `Markdown`.
+  """
+  @type parse_mode :: String.t()
+  @type sendMessageOption :: [
+          parse_mode: parse_mode,
+          disable_web_page_preview: boolean,
+          disable_notification: boolean,
+          reply_to_message_id: integer
+        ]
+
+  @spec sendMessage(String.t() | integer, String.t(), sendMessageOption) :: telegram_response
   def sendMessage(chat_id, text, options \\ []) do
-    body = options ++ [chat_id: chat_id, text: text]
-    |> Enum.into(%{})
+    body =
+      (options ++ [chat_id: chat_id, text: text])
+      |> Enum.into(%{})
+
     http("/sendMessage", body)
   end
 
+  @doc """
+  Get basic info about a file and prepare it for downloading
+  """
+  @spec getFile(String.t()) :: telegram_response
+  def getFile(file_id) do
+    http("/getFile", %{file_id: file_id})
+  end
 end
